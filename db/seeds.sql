ebe1da6238edc4c8444d4847d2f35d17ae5886fa
Charlie Bowden <charles.bowden@gmail.com>
2018-01-17 18:19:11 -0500

    Orm Updated

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Starting Records for burgers_db
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shroom n Swiss', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smoke & Pepper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger', false);
