f0a5475a531e0fc0312a64e0142bc1dbd950d737
Charlie Bowden <charles.bowden@gmail.com>
2018-01-19 20:46:37 -0500

    MVP + CSS linkage

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,7 +20,8 @@ router.post("/api/burgers", function(req, res) {
   burger.insertOne(["burger_name"], [req.body.burger_name], function(result) {
     // Send back the ID of the new quote
     console.log(req.body);
-    res.json({ id: result.insertId });
+    // res.json({ id: result.insertId });
+    res.redirect("/");
   });
 });
 
