1ed9801422a03f15f6441cc87e601d0723efcf02
ctbowden <charles.bowden@gmail.com>
2018-01-20 20:15:46 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "3r3bosis1337!",
+  password: "",
   database: "burgers_db"
 });
 };
